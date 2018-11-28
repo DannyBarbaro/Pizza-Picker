56865755330d4ea67442731caff4b5c1b010a45d
Danny <danny@MacBook-Pro-15.local>
2018-11-28 00:53:19 -0500

    finished the inital page and added routing

--- pizza-picker/src/app/app-routing.module.ts
+++ pizza-picker/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { UserHomepageComponent } from './user-homepage/user-homepage.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: 'home', component: HomeComponent },
+  { path: 'userpage', component: UserHomepageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
