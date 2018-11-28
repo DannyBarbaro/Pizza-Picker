0da726a6e70c889ad5acd9941844a10d5ac0bec2
Josh Levy <jml268@case.edu>
2018-11-28 01:52:13 -0500

    fancy setup stuff

--- db-setup.sql
+++ db-setup.sql
@@ -1,202 +1,64 @@
--- MySQL dump 10.13  Distrib 5.7.24, for Linux (x86_64)
---
--- Host: localhost    Database: pizza
--- ------------------------------------------------------
--- Server version	5.7.24-0ubuntu0.16.04.1
+-- setup database and user
+drop database if exists pizza;
+create database pizza;
+drop user if exists 'sommelier'@'localhost';
+create user 'sommelier'@'localhost' identified by 'mamamia';
+grant all on pizza.* to 'sommelier'@'localhost';
+flush privileges;
+use pizza;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- create tables
+CREATE TABLE User (
+  username varchar(32) NOT NULL,
+  password varchar(255) NOT NULL,
+  PRIMARY KEY (username)
+);
 
---
--- Table structure for table `Allergy`
---
+CREATE TABLE Topping (
+  name varchar(32) NOT NULL,
+  PRIMARY KEY (name)
+);
 
-DROP TABLE IF EXISTS `Allergy`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Allergy` (
-  `user` varchar(32) NOT NULL,
-  `topping` varchar(32) NOT NULL,
-  PRIMARY KEY (`user`,`topping`),
-  KEY `topping` (`topping`),
-  CONSTRAINT `Allergy_ibfk_1` FOREIGN KEY (`user`) REFERENCES `User` (`username`),
-  CONSTRAINT `Allergy_ibfk_2` FOREIGN KEY (`topping`) REFERENCES `Topping` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE Allergy (
+  user varchar(32) NOT NULL,
+  topping varchar(32) NOT NULL,
+  PRIMARY KEY (user, topping),
+  FOREIGN KEY (user) REFERENCES User(username),
+  FOREIGN KEY (topping) REFERENCES Topping(name)
+);
 
---
--- Dumping data for table `Allergy`
---
+CREATE TABLE Food_Order (
+  order_id int(11) NOT NULL AUTO_INCREMENT,
+  user varchar(32) NOT NULL,
+  date datetime,
+  PRIMARY KEY (order_id),
+  FOREIGN KEY (user) REFERENCES User(username)
+);
 
-LOCK TABLES `Allergy` WRITE;
-/*!40000 ALTER TABLE `Allergy` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Allergy` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE Friends (
+  friend1 varchar(32) NOT NULL,
+  friend2 varchar(32) NOT NULL,
+  PRIMARY KEY (friend1, friend2),
+  FOREIGN KEY (friend1) REFERENCES User(username),
+  FOREIGN KEY (friend2) REFERENCES User(username)
+);
 
---
--- Table structure for table `Food_Order`
---
+CREATE TABLE Preference_Set (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  user varchar(32) NOT NULL,
+  title varchar(32) DEFAULT NULL,
+  is_default tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user) REFERENCES User(username)
+);
 
-DROP TABLE IF EXISTS `Food_Order`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Food_Order` (
-  `order_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user` varchar(32) NOT NULL,
-  PRIMARY KEY (`order_id`),
-  KEY `user` (`user`),
-  CONSTRAINT `Food_Order_ibfk_1` FOREIGN KEY (`user`) REFERENCES `User` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE Preference (
+  topping varchar(32) NOT NULL,
+  set_id int(11) NOT NULL,
+  score int(11) NOT NULL,
+  PRIMARY KEY (topping,set_id),
+  FOREIGN KEY (set_id) REFERENCES Preference_Set(id)
+);
 
---
--- Dumping data for table `Food_Order`
---
-
-LOCK TABLES `Food_Order` WRITE;
-/*!40000 ALTER TABLE `Food_Order` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Food_Order` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Friends`
---
-
-DROP TABLE IF EXISTS `Friends`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Friends` (
-  `friend1` varchar(32) NOT NULL,
-  `friend2` varchar(32) NOT NULL,
-  PRIMARY KEY (`friend1`,`friend2`),
-  KEY `friend2` (`friend2`),
-  CONSTRAINT `Friends_ibfk_1` FOREIGN KEY (`friend1`) REFERENCES `User` (`username`),
-  CONSTRAINT `Friends_ibfk_2` FOREIGN KEY (`friend2`) REFERENCES `User` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Friends`
---
-
-LOCK TABLES `Friends` WRITE;
-/*!40000 ALTER TABLE `Friends` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Friends` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Preference`
---
-
-DROP TABLE IF EXISTS `Preference`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Preference` (
-  `topping` varchar(32) NOT NULL,
-  `set_id` int(11) NOT NULL,
-  `score` int(11) NOT NULL,
-  PRIMARY KEY (`topping`,`set_id`),
-  KEY `set_id` (`set_id`),
-  CONSTRAINT `Preference_ibfk_1` FOREIGN KEY (`set_id`) REFERENCES `Preference_Set` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Preference`
---
-
-LOCK TABLES `Preference` WRITE;
-/*!40000 ALTER TABLE `Preference` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Preference` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Preference_Set`
---
-
-DROP TABLE IF EXISTS `Preference_Set`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Preference_Set` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user` varchar(32) NOT NULL,
-  `title` varchar(32) DEFAULT NULL,
-  `is_default` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user` (`user`),
-  CONSTRAINT `Preference_Set_ibfk_1` FOREIGN KEY (`user`) REFERENCES `User` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Preference_Set`
---
-
-LOCK TABLES `Preference_Set` WRITE;
-/*!40000 ALTER TABLE `Preference_Set` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Preference_Set` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Topping`
---
-
-DROP TABLE IF EXISTS `Topping`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Topping` (
-  `name` varchar(32) NOT NULL,
-  PRIMARY KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Topping`
---
-
-LOCK TABLES `Topping` WRITE;
-/*!40000 ALTER TABLE `Topping` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Topping` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `User`
---
-
-DROP TABLE IF EXISTS `User`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `User` (
-  `username` varchar(32) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `User`
---
-
-LOCK TABLES `User` WRITE;
-/*!40000 ALTER TABLE `User` DISABLE KEYS */;
-/*!40000 ALTER TABLE `User` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-11-27 21:59:36
+-- prepare queries
+prepare get_history from 'select order_id, date from Food_Order where user = ?';
