4826a75e6ffd95a347833e48c301ddeb3e9e560e
cgaff623 <cgaff623@gmail.com>
2018-11-29 02:19:38 -0500

    Id like you to meet this set of my home slices that I can make now

--- pizz_pick.py
+++ pizz_pick.py
@@ -34,6 +34,7 @@ def sql_execute(sql):
 # routing user to different things
 @app.route('/availableUser/<username>')
 def check_user(username):
+    print(request.form)
     # query database for if the user under username exists
     # if exists, return true
     # else, return false
@@ -52,12 +53,14 @@ def make_pizzas(prefs_list):
     rounded down to the nearest multiple of 2 with a minimum of 8.  The acceptable
     range of sizes are small (8), medium (10), large (12), and extra large 14).
 
-    Returns a list of pizzas.  Each pizza is a list of tuples containing the
-    toppings on a slice and how many slices have those toppings.
+    Returns a list of pairs containing the set of toppings on a slice and how
+    many of those
     """
-    num_slices = min(math.floor((3.5 * len(prefs_list))/2)*2, 8)
+    num_slices = max(math.floor((3.5 * len(prefs_list))/2)*2, 8)
+    slice_per_person = num_slices / len(prefs_list)
+    leftovers = num_slices % len(prefs_list)
 
-    # dict that matches topping name to a (num_likes, num_dislikes) pair
+    # dict that matches topping name to a num_likes-num_dislikes difference
     topping_yummy = {}
     bad_tops = []
     # for each person's preference list
@@ -76,25 +79,78 @@ def make_pizzas(prefs_list):
                     del topping_yummy[top_yum[0]]
             # if someone else already had a preference for the topping
             elif top_yum[0] in topping_yummy:
-                # if this person likes the topping
-                if top_yum[1] > 0:
-                    # increment num_likes
-                    topping_yummy[top_yum[0]] = (topping_yummy[top_yum[0]][0]+1, topping_yummy[top_yum[0]][1])
-                # if this person doesn't like the topping
-                else:
-                    # increment num_dislikes
-                    topping_yummy[top_yum[0]] = (topping_yummy[top_yum[0]][0], topping_yummy[top_yum[0]][1]+1)
+                topping_yummy[top_yum[0]] += top_yum[1]
             # if no one has mentioned the topping before
             else:
-                if top_yum[1] > 0:
-                    topping_yummy[top_yum[0]] = (1, 0)
-                else:
-                    topping_yummy[top_yum[0]] = (0, 1)
+                topping_yummy[top_yum[0]] = top_yum[1]
 
+    standard_top_order = []
+    best_topping = ("", -999999)
+    for topping in topping_yummy:
+        standard_top_order.append(topping)
+        if topping_yummy[topping] > best_topping[1]:
+            best_topping = (topping, topping_yummy[topping])
+    best_topping = best_topping[0]
+
+    # make standard ordered and sanatized vectors of yumminess quotients for each preference
     pref_vecs = []
     for pref in prefs_list:
         pref_vecs.append([])
+        for topping in topping_yummy:
+            if (topping, -1) in pref:
+                pref_vecs[-1].append(-1)
+            elif (topping, 1) in pref:
+                pref_vecs[-1].append(1)
+            else:
+                pref_vecs[-1].append(0)
+        # a value to track how many people agree with this topping set
+        pref_vecs[-1].append(1)
 
+    used_indices = []
+    num_combos = len(pref_vecs)
+
+    i = 0
+    while i < len(pref_vecs):
+        if i in used_indices:
+            i += 1
+            continue
+        j = 0
+        while j < len(pref_vecs):
+            if i in used_indices:
+                break
+            if i == j or j in used_indices:
+                j += 1
+                continue
+            combo_vec = []
+            good_combo = True
+            for k in range(len(pref_vecs[0])):
+                combo_vec.append(pref_vecs[i][k] + pref_vecs[j][k])
+                # if the two preferences have some conflict of interest: one like, the other dislike
+                if abs(combo_vec[k]) < abs(pref_vecs[i][k]) or abs(combo_vec[k]) < abs(pref_vecs[j][k]):
+                    good_combo = False
+                    break
+            if good_combo:
+                used_indices.extend([i, j])
+                pref_vecs.append(combo_vec)
+                num_combos -= 1
+            j += 1
+        i += 1
+
+    slice_set = []
+    for i in range(len(pref_vecs)):
+        if i in used_indices:
+            continue
+        topping_list = []
+        for topping, j in standard_top_order, range(len(pref_vecs[0])-1):
+            if pref_vecs[j] > 0:
+                topping_list.append(topping)
+        slice_set.append((topping_list, slice_per_person * pref_vecs[i][-1]))
+
+    if leftovers > 0:
+        # fill in the rest with the most popular topping
+        slice_set.append(((best_topping), leftovers))
+
+    return slice_set
 
 #@app.route('/', methods=['GET', 'POST'])
 def template_response_with_data():
