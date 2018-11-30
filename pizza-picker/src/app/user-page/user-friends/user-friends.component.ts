6ff2325735dd8eb82e36e3c4de709081d542b204
Danny <danny@MacBook-Pro-15.local>
2018-11-29 21:36:37 -0500

    adding friends web calls

--- pizza-picker/src/app/user-page/user-friends/user-friends.component.ts
+++ pizza-picker/src/app/user-page/user-friends/user-friends.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-friends',
@@ -9,18 +11,37 @@ export class UserFriendsComponent implements OnInit {
 
   friendAddResult: String = "";
   addField: String = "";
+  friends: String[] = [];
 
-  constructor() { }
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
+    this.fetchFriends();
   }
 
-  addFriend(){
+  fetchFriends() {
+    this.http.get<String[]>('http://localhost:8080/friends/' + this.router.url.split('/')[2])
+      .subscribe((data: String[]) => {
+        this.friends = data;
+      });
+  }
 
+  addFriend() {
+    if (this.addField.length != 0) {
+      this.http.get<Boolean>('http://localhost:8080/userExists/' + this.router.url.split('/')[2])
+        .subscribe((data: Boolean) => {
+          if (data) {
+            this.http.post('http://localhost:8080/friend/' + this.router.url.split('/')[2] + this.addField, "").subscribe(x => this.fetchFriends())
+          }
+        });
+    }
   }
 
   unfriend(name: String) {
-    
+    this.http.delete('http://localhost:8080/unfriend/' + this.router.url.split('/')[2] + name)
+      .subscribe(x => {
+        this.fetchFriends();
+      });
   }
 
 }
